d48220cea0b90c7a8c478d0cf19a0adf7ad77e06
nodlya <nadya-popova-02@mail.ru>
2023-05-22 09:05:02 +0500

    Improved elevator logic (smooth movement)

--- src/components/SchematicElevator.vue
+++ src/components/SchematicElevator.vue
@@ -2,8 +2,8 @@
     <div class="elevator-app">
         <div class="elevator">
             <div v-for="floor in floors" :key="floor" class="elevator-floor">
-                <button class="elevator-floor-order" @click="requestFloor(floor)"></button>
-                <p>{{ waiting[floors.indexOf(floor)] }}</p>
+                <button v-if="floor>1" class="elevator-floor-order" @click="requestFloor(floor)"></button>
+                <p v-if="floor>1">{{ waiting[floors.indexOf(floor)] }}</p>
                 <div class="elevator-cabin" v-if="currentFloor == floor">
                     <p>{{ peopleOnElevator }}</p>
                 </div>
@@ -14,6 +14,7 @@
 </template>
 
 <script>
+
 export default {
   data() {
     return {
@@ -22,9 +23,7 @@ export default {
       floors: [5, 4, 3, 2, 1],
       peopleOnElevator: 0,
       maxFloor: 1,
-      isMoving: false,
-      hasUnpickedPeople: false,
-      hasUnpickedPeopleAbove: false 
+      isMoving: false
     };
   },
   watch: {
@@ -44,16 +43,9 @@ export default {
       handler(floor) {
         if (floor === 1) {
           this.peopleOnElevator = 0;
-          if (this.hasUnpickedPeople) {
-            const unpickedFloor = this.getUnpickedFloor();
-            if (unpickedFloor !== -1) {
-              this.moveElevator(unpickedFloor);
-            }
-          } else if (this.hasUnpickedPeopleAbove) {
-            const highestUnpickedFloor = this.getHighestUnpickedFloorAbove();
-            if (highestUnpickedFloor !== -1) {
-              this.moveElevator(highestUnpickedFloor);
-            }
+          const unpickedFloor = this.getUnpickedFloor();
+          if (unpickedFloor !== -1) {
+            this.moveElevator(unpickedFloor);
           }
         }
       }
@@ -62,18 +54,14 @@ export default {
   methods: {
     requestFloor(floor) {
       this.waiting[this.floors.indexOf(floor)]++;
-      if (floor > this.maxFloor) this.maxFloor = floor;
-      this.hasUnpickedPeople = true; 
-      if (floor > this.currentFloor) {
-        this.hasUnpickedPeopleAbove = true; 
-      }
+      this.maxFloor = Math.max(this.maxFloor, floor);
     },
     moveElevator(floor) {
       if (this.isMoving) return;
       this.isMoving = true;
 
       const delay = 1000;
-      const direction = floor > this.currentFloor ? 1 : -1;
+      const direction = Math.sign(floor - this.currentFloor);
 
       if (floor === this.currentFloor) {
         this.isMoving = false;
@@ -88,8 +76,6 @@ export default {
           this.isMoving = false;
           this.peopleOnElevator += this.waiting[this.floors.indexOf(this.currentFloor)];
           this.waiting[this.floors.indexOf(this.currentFloor)] = 0;
-          this.hasUnpickedPeople = false; 
-          this.hasUnpickedPeopleAbove = false; 
         } else {
           this.peopleOnElevator += this.waiting[this.floors.indexOf(this.currentFloor)];
           this.waiting[this.floors.indexOf(this.currentFloor)] = 0;
@@ -107,7 +93,7 @@ export default {
 
       const interval = setInterval(moveStep, delay);
     },
-    getUnpickedFloor() { 
+    getUnpickedFloor() {
       for (let i = this.currentFloor + 1; i <= this.maxFloor; i++) {
         if (this.waiting[this.floors.indexOf(i)] > 0) {
           return i;
@@ -122,17 +108,10 @@ export default {
         }
       }
       return -1;
-    },
-    getHighestUnpickedFloorAbove() {
-      for (let i = this.maxFloor; i > this.currentFloor; i--) {
-        if (this.waiting[this.floors.indexOf(i)] > 0) {
-          return i;
-        }
-      }
-      return -1;
     }
   }
 };
+
 
 </script>
 
